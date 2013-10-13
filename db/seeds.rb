28d9118f01d0f7fbdb78aba609b73f55ca06ed34
EC2 Default User <ec2-user@ip-10-185-56-141.ec2.internal>
2013-10-13 06:18:16 +0000

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,44 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-file = File.new("db/page.sql", "r")
-49.times { file.gets } # skip the first 49 lines
+#file = File.new("db/enwiki-latest-page.sql", "r")
+#49.times { file.gets } # skip the first 49 lines
+#
+#line = file.gets
+#line[0..line.index(' (')-1] = ''
+#10_000.times do
+#	match = line.match /\(([0-9]*),[0-9]*,'([^']*)'.*\)[,;]$/
+#	id, title = match[1], match[2]
+#	page = Page.new(id: id ,title: title)
+#	page.save if page.valid?
+#	line = file.gets
+#end
+#file.close
+#puts "Pages: #{Page.count}"
 
-line = file.gets
-line[0..line.index(' (')-1] = ''
-begin
-	match = line.match /\(([0-9]*),[0-9]*,'([^']*)'.*\)[,;]$/
-	id, title = match[1], match[2]
-	Page.create(id: id ,title: title)
-end while (line = file.gets)
-file.close
-puts "Pages: #{Page.count}"
-
-file =File.new("db/pagelinks.sql","r")
+file =File.new("db/enwiki-latest-pagelinks.sql","r")
 38.times { file.gets } # skip the first 49 lines
 
 line = file.gets
 line[0..line.index(' (')] = ''
-begin
+n = 0
+while(n < 10_000)
+	n += 1
 	match = line.match /\(([0-9]*),[0-9]*,'(.*)'\)[,;]$/
 	id, title = match[1], match[2]
 	
-	to_id=Page.find_by_title(title).id
-	Link.create(to_id: to_id, from_id: id)
-end while (line = file.gets)
+	begin
+		to_id=Page.find_by_title(title).id
+		from_id=Page.find_by_id(id).id
+		link = Link.create(to_id: to_id, from_id: id)
+		if link.valid?
+			link.save
+		else
+			n -= 1
+		end
+	rescue
+		n -= 1
+	end
+	line = file.gets
+end
 puts "Links: #{Link.count}"
